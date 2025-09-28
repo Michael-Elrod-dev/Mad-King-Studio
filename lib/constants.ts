d893c7a7e9101e581bc8fb941ff1e26fd3d91c69
Michael-Elrod-dev <michaelelrod.dev@gmail.com>
2025-09-28 01:51:55 -0400

    Discord link update

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 // lib/constants.ts
 export const SOCIAL_LINKS = {
   TWITCH: "https://www.twitch.tv/aimosthadme",
-  DISCORD: "https://discord.gg/XEwUXAv",
+  DISCORD: "https://discord.gg/JSH5ct2nAC",
   X: "https://x.com/MadKingStudio",
 } as const;
